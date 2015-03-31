c24ccfe845fdab76fe870f14d1d2cfe6db1118e8
Hannah Nordgren <hey@hereshannahs.info>
2015-03-31 13:16:14 -0400

    started in on collapsing nav.

--- js/basics.js
+++ js/basics.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
   console.log('the basics have loaded');
 
+  //change function for touch events, not click.
+  function toggleMenu(){
+    $('.dropdown-button').show();
+    $('.dropdown-button').click(function(){
+      $('.dropdown-menu').slideToggle();
+
+    })
+
+  }
+
   function checkWidth() {
       var windowSize = $(window).width();
 
       if ($(window).width() >= 1 && $(window).width() <=768) {
        console.log('smallest');
         $('.nav-links').hide();
+        toggleMenu();
+      } else {
+        $('.nav-links').show();
       }
     }
 
