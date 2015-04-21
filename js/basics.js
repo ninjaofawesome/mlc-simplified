71739778be7cdf94cc272c1913355d569edeb1b3
Hannah Nordgren <hey@hereshannahs.info>
2015-04-21 12:47:21 -0400

    FINALLY the FAQ is done

--- js/basics.js
+++ js/basics.js
@@ -31,58 +31,25 @@ $(document).ready(function(){
 
     function faqButton(){
       var answer = $('.answer');
-      var up = $('.arrow-icon.up');
-      var buttonId= $(this).id;
-      var down = $('.questionBox');
-      var icon=$('.arrow-icon.down').attr('id');
-      var iconUp=$('.arrow-icon.up').attr('id');
-      var buttonId= $(this).id;
-      var slideUp = $('.button-slide-up', '.arrow-icon.up');
       var buttonUp = $('.button-slide-up');
 
       answer.hide();
       buttonUp.hide();
-      up.hide();
 
       $('.questionBox').click(function(){
         var myBox = $(this);
-        myBox.children('.answer').slideToggle();
-
-        // var myId = $(this).attr('id');
-        // var myBox= $(this).attr('class');
-        // var myItem = myBox + myId;
-        // myBox.children('.answer').show();
+        myBox.children('.answer').slideToggle(function(){
+          if('.answer:visible' == true) {
+            console.log('yay');
+            buttonUp.hide();
+          } else {
+            console.log('boo');
+            buttonUp.show();
+          }
+        });
       });
 
-      // $('.button-slide-up').click(function(){
-      //   $(this).parent('.answer').slideUp();
-      //   // answer.removeClass('show-me');
-      //   // $('.button-slide-up').fadeOut();
-      //   // answer.slideUp(600, function(){
-      //   //   up.fadeOut(400);
-      //   //   down.delay(400).fadeIn();
 
-      //   // });
-      // });
-
-      // $('.button-slide-up').hide();
-
-
-
-
-
-      // $('.question-arrow').click(function(){
-      //   $('.question-arrow').parent().find();
-      //   up.delay(600).fadeIn();
-      //   down.fadeOut(600);
-      //   answer.slideToggle(600);
-      // });
-
-
-
-      // icon.click(function(){
-      //   alert('hi');
-      // });
 
     }
 
